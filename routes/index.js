147ec66c95eb3fb9aecb9060d4f1c459f6dc45a2
Putnam14 <putnambr76@gmail.com>
2018-01-14 16:27:08 -0800

    Fix books page with trailing slash

--- routes/index.js
+++ routes/index.js
@@ -28,8 +28,75 @@ router.post('/book-search', function(req, res, next) {
     if (results == 0) {
       res.redirect('/404');
     } else {
-      res.redirect('/book/isbn-' + results);
+      res.redirect('/book/isbn-' + results + '/');
     };
+  });
+});
+
+/* GET books page */
+router.get('/book/isbn-:urlISBN(\\d{9}[\\d|X]$)/', function(req, res, next) {
+  var ISBN = req.params['urlISBN'];
+  if (ISBN == '404') {
+    res.redirect('/404');
+  }
+  bookSearch.getBook(config.featuredBook.ISBN, function(featured) {
+    bookSearch.getBook(ISBN, function(results) {
+      if (!results) {
+        res.redirect('/404');
+      } else {
+        //res.status(200).send(results); //JSON
+        bookSearch.getBookPrice(ISBN, function(price) {
+          var WPM = config.wordsPerMinuteReading;
+          var estMin = (results.Wordcount / WPM);
+          var avgEstHr = Math.floor(estMin / 60);
+          var avgEstMin = Math.floor(estMin - (avgEstHr * 60));
+          switch (results.Accuracy) { //Accuracy dot selection. Ugly. Please fix.
+            case 'Verified':
+              var dot = 'dot dot-success';
+              break;
+            case 'Estimate':
+              var dot = 'dot dot-warning';
+              break;
+            default:
+              var dot = 'dot dot-danger';
+          }
+          res.render('books', {
+            pageTitle: results.Title,
+            description: "The average reader will take " + avgEstHr + " hours, " + avgEstMin + " minutes to read " + results.Title + " at a speed of 250 WPM. Estimated " + results.Wordcount + " words in " + results.Pages + " pages. Find out how long it will take you to read it!",
+            avgEstHr: avgEstHr,
+            avgEstMin: avgEstMin,
+            WPM: WPM,
+            ref: config.amazonAPI.associatesTag,
+            ISBN: results.ISBN,
+            Title: results.Title,
+            Author: results.Author,
+            MSRP: results.MSRP,
+            Pages: results.Pages.toLocaleString(),
+            Wordcount: results.Wordcount.toLocaleString(),
+            Dot: dot,
+            Accuracy: results.Accuracy,
+            AccuracyDesc: results.AccuracyDesc,
+            ReleaseDate: results.ReleaseDate,
+            Sim1ISBN: results.Sim1ISBN, 
+            Sim1Title: results.Sim1Title,
+            Sim2ISBN: results.Sim2ISBN,
+            Sim2Title: results.Sim2Title,
+            Sim3ISBN: results.Sim3ISBN,
+            Sim3Title: results.Sim3Title,
+            Image: results.Image,
+            Description: results.Description.replace(/<(?:.|\n)*?>/gm, ''),
+            Price: price,
+            FeaturedMonth: config.featuredBook.month,
+            FeaturedTitle: featured.Title,
+            FeaturedImage: featured.Image,
+            FeaturedISBN: featured.ISBN,
+            FeaturedWordcount: featured.Wordcount.toLocaleString(),
+            adsDiv: config.responsiveAdId,
+            adsSrc: config.responsiveAdSrc
+          });
+        });
+      };
+    });
   });
 });
 
