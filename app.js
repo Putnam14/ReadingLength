b3486bec236aa281a22f9911738ce2a03d38cf32
Putnam14 <putnam@Bridgers-MacBook-Pro.local>
2018-10-29 21:14:12 -0600

    Format with Prettier

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-var express = require('express');
-var path = require('path');
-//var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var expressSanitizer = require('express-sanitizer');
-var bodyParser = require('body-parser');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const expressSanitizer = require('express-sanitizer')
+const bodyParser = require('body-parser')
 
-var index = require('./routes/index');
+const index = require('./routes/index')
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-//Test
-//app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(expressSanitizer());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Test
+// app.use(logger('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(expressSanitizer())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index);
+app.use('/', index)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  res.redirect('/404');
-  next(err);
-});
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  res.redirect('/404')
+  next(err)
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
